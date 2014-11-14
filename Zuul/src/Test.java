7936a3288b6ac281dc735caea4ff929f063634d6
Melchor Jérémy <patchimou14@gmail.com>
2014-11-14 17:13:31 +0100

    Nouveau commit pour Terminal

--- Zuul/src/Test.java
+++ Zuul/src/Test.java
@@ -13,12 +13,6 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		LOL. fetch -> pull
-=======
-		ojugaereouhgfez;
-		jhuu
->>>>>>> parent of 5085cc9... Update Test.java
-	}
+
 
 }
