ab92b5d43fea1df1f2312de2548a25fb4a79ebe0
Melchor Jérémy <patchimou14@gmail.com>
2014-11-25 23:17:19 +0100

    Version avec les command originaux mais ne marchent pas. Sauvegarde avant grosse modification du package command

--- Zuul/src/main/Game.java
+++ Zuul/src/main/Game.java
@@ -1,5 +1,6 @@
 package main;
 
+import command.*;
 import rooms.*;
 import player.*;
 import things.*;
@@ -7,9 +8,11 @@ import things.*;
 public class Game {
 	Room currentRoom;
     public Player player;
+    private Parser parser;
 	
     public Game() throws InterruptedException {
     	Player player = new Player();
+    	parser = new Parser();
     }
     
     /**
@@ -59,4 +62,119 @@ public class Game {
         return player;
     }
     
+    /**
+     * Main play routine. Loops until end of play.
+     */
+    public void play() {
+    	createRooms();
+        printWelcome();
+
+        // Enter the main command loop. Here we repeatedly read commands and
+        // execute them until the game is over.
+
+        boolean finished = false;
+        while (!finished) {
+			Command command = parser.getCommand();
+            finished = processCommand(command);
+        }
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome() {
+        System.out.println();
+        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
+        System.out.println();
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    /**
+     * Given a command, process (that is: execute) the command.
+     * 
+     * @param command
+     *            The command to be processed.
+     * @return true If the command ends the game, false otherwise.
+     */
+    private boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        CommandWord commandWord = command.getCommandWord();
+
+        switch (commandWord) {
+        case UNKNOWN:
+            System.out.println("I don't know what you mean...");
+            break;
+
+        case HELP:
+            printHelp();
+            break;
+
+        case EAST:
+            goRoom(command);
+            break;
+            
+        case WEST:
+            goRoom(command);
+            break;
+        
+        case NORTH:
+            goRoom(command);
+            break;
+           
+        case SOUTH:
+            goRoom(command);
+            break;
+
+        case QUIT:
+            wantToQuit = quit(command);
+            break;
+        }
+        return wantToQuit;
+    }
+
+     //implementations of user commands:
+
+    /**
+     * Print out some help information. Here we print some stupid, cryptic
+     * message and a list of the command words.
+     */
+    private void printHelp() {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("around at the university.");
+        System.out.println();
+        System.out.println("Your command words are:");
+        parser.showCommands();
+    }
+
+    /**
+     * Try to go in one direction. If there is an exit, enter the new room,
+     * otherwise print an error message.
+     */
+    private void goRoom(Command command) {
+        String direction = command.getCommandWord();
+
+         //Try to leave current room.
+        Room nextRoom = currentRoom.getExit(direction);
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        } else {
+            currentRoom = nextRoom;
+            System.out.println(currentRoom.getLongDescription());
+        }
+    }
+
+    /**
+     * "Quit" was entered. Check the rest of the command to see whether we
+     * really quit the game.
+     * 
+     * @return true, if this command quits the game, false otherwise.
+     */
+    private boolean quit(Command command) {
+            return true; // signal that we want to quit
+    }    
 }
