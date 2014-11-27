f45dfe0bcbda2a3106a6eca1e0d98cd114ca1e14
Melchor Jérémy <patchimou14@gmail.com>
2014-11-27 22:54:17 +0100

    J'emmerde les conflits

---
+++ Zuul/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import player.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+    	Game a = new Game();
+    	a.play();
+
+    }
+}
